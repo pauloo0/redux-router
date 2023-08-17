b94952d19c0387598953471515fdc88bd085691d
Paulo Oliveira <oliveirapaulo00@hotmail.com>
2023-08-17 12:06:42 +0100

    Adding TimeAgo component to see how long a post was made

--- src/features/posts/PostsList.tsx
+++ src/features/posts/PostsList.tsx
@@ -3,6 +3,7 @@ import { selectAllPosts } from './postsSlice'
 import { useSelector } from 'react-redux'
 
 import PostAuthor from './PostAuthor'
+import TimeAgo from './TimeAgo'
 
 const PostsList: React.FC = () => {
   const posts = useSelector(selectAllPosts)
@@ -13,6 +14,7 @@ const PostsList: React.FC = () => {
       <p className='text-xl font-light'>{post.content.substring(0, 100)}</p>
       <p>
         <PostAuthor userId={post.userId} />
+        <TimeAgo timestamp={post.date} />
       </p>
     </article>
   ))
--- src/features/posts/postsSlice.ts
+++ src/features/posts/postsSlice.ts
@@ -2,11 +2,14 @@ import { createSlice, nanoid } from '@reduxjs/toolkit'
 import type { PayloadAction } from '@reduxjs/toolkit'
 import type { RootState } from '../../app/store'
 
+import { sub } from 'date-fns' // date-fns is a library that provides functions for manipulating dates
+
 interface Post {
   id: string
   title: string
   content: string
   userId?: string
+  date: string
 }
 
 interface PostsState {
@@ -19,11 +22,13 @@ const initialState: PostsState = {
       id: '1',
       title: 'Learning Redux Toolkit',
       content: "I've heard good things.",
+      date: sub(new Date(), { minutes: 10 }).toISOString(),
     },
     {
       id: '2',
       title: 'Slices...',
       content: 'The more I say slice, the more I want pizza.',
+      date: sub(new Date(), { minutes: 5 }).toISOString(),
     },
   ],
 }
@@ -42,6 +47,7 @@ const postsSlice = createSlice({
             id: nanoid(), // nanoid is a function from redux toolkit that generates a unique ID string
             title,
             content,
+            date: new Date().toISOString(),
             userId,
           },
         }
