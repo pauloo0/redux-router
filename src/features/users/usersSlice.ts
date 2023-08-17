2f609f4ca158a7e1e452c0709c80f03e539c1de9
Paulo Oliveira <oliveirapaulo00@hotmail.com>
2023-08-17 14:53:31 +0100

    Refactoring app to fetch users from API and dispatching when website loads

--- src/features/users/usersSlice.ts
+++ src/features/users/usersSlice.ts
@@ -1,5 +1,9 @@
-import { createSlice } from '@reduxjs/toolkit'
-import { PayloadAction } from '@reduxjs/toolkit'
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import axios from 'axios'
+
+const USERS_URL = 'https://jsonplaceholder.typicode.com/users'
+
+// import { PayloadAction } from '@reduxjs/toolkit'
 import type { RootState } from '../../app/store'
 
 interface User {
@@ -12,17 +16,29 @@ interface UsersState {
 }
 
 const initialState: UsersState = {
-  users: [
-    { id: '0', name: 'Tianna Jenkins' },
-    { id: '1', name: 'Kevin Grant' },
-    { id: '2', name: 'Madison Price' },
-  ],
+  users: [],
 }
+
+export const fetchUsers = createAsyncThunk('users/fetchUsers', async () => {
+  try {
+    const response = await axios.get(USERS_URL)
+    return response.data
+  } catch (error) {
+    if (error instanceof Error) {
+      return error.message
+    }
+  }
+})
 
 const usersSlice = createSlice({
   name: 'users',
   initialState,
   reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchUsers.fulfilled, (state, action) => {
+      state.users = state.users.concat(action.payload)
+    })
+  },
 })
 
 export const selectAllUsers = (state: RootState) => state.users.users
--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,10 @@ import './styles/style.css'
 import { store } from './app/store.ts'
 import { Provider } from 'react-redux'
 
+import { fetchUsers } from './features/users/usersSlice.ts'
+
+store.dispatch(fetchUsers())
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
