5c85afe6e8313cb948ba7cc6113a08a0192e17b1
Paulo Oliveira <oliveirapaulo00@hotmail.com>
2023-08-19 16:07:00 +0100

    Adding catch all route for 404 pages

--- src/App.tsx
+++ src/App.tsx
@@ -22,18 +22,22 @@ import UserPage from './features/users/UserPage'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path='/' element={<RootLayout />}>
-      <Route index element={<PostsList />} />
-      <Route path='post'>
-        <Route index element={<AddPostForm />} />
-        <Route path=':postId' element={<SinglePostPage />} />
-        <Route path='edit/:postId' element={<EditPostForm />} />
+    <>
+      <Route path='/' element={<RootLayout />}>
+        <Route index element={<PostsList />} />
+        <Route path='post'>
+          <Route index element={<AddPostForm />} />
+          <Route path=':postId' element={<SinglePostPage />} />
+          <Route path='edit/:postId' element={<EditPostForm />} />
+        </Route>
+        <Route path='user'>
+          <Route index element={<UsersList />} />
+          <Route path=':userId' element={<UserPage />} />
+        </Route>
       </Route>
-      <Route path='user'>
-        <Route index element={<UsersList />} />
-        <Route path=':userId' element={<UserPage />} />
-      </Route>
-    </Route>
+
+      <Route path='*' element={<h1>Page not found</h1>} />
+    </>
   )
 )
 
